3f90633b8753b556ae45dac157dd9854dd013d47
sunilyadav489 <sunilyadav489@gmail.com>
2023-02-28 15:29:07 +0530

    adding initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+// const path = require('path');
+// const webpack = require('webpack');
+
+// module.export = {
+//     //entry object where webpack looks to start building the bundle
+//     entry: './src/index.js',
+//     //webpack in development
+//     mode: 'development',
+//     module: {
+//         rules:[
+//             // transform ES6 to common js
+//             {
+//                 test: /\.(js|jsx)$/,
+//                 exclude: /(node_modules)/,
+//                 loader : 'babel-loader',
+//                 options: { presets: ['@babel/env']}
+//             },
+//             {
+//                 test: /\.css$/,
+//                 use: ["style-loader", "css-loader"]
+//             }
+//         ]
+//     },
+//     resolve: {extensions:['*', 'js', 'jsx']},
+//     output: {
+//         path: path.resolve(__dirname, 'dist/'),
+//         publicPath: '/dist/',
+//         filename: 'bundle.js'
+//     },
+//     deveServer: {
+//         contentBase: path.join(__dirname, 'public/'),
+//         port: 3000,
+//         publicPath: 'http://localhost:3000/dist',
+//         hotOnly: true
+//     },
+//     plugins: [ new webpack.HotModuleReplacementPlugin()]
+// };
+
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        // contentBase: path.join(__dirname, 'public/'),
+        static : {
+            directory : path.join(__dirname, "public/")
+        },
+        port: 3000,
+        // publicPath: 'http://localhost:3000/dist/',
+        devMiddleware:{
+            publicPath: "https://localhost:3000/dist/",
+        },
+        // hotOnly: true
+        // hotOnly
+        hot: "only",
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+};
